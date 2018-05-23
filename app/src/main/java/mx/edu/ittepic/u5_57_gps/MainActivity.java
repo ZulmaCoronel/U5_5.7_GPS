169a96e136a5ffe2dd13d7e5324db5fa036a2d86
Zulma <zuiscoronello@ittepic.edu.mx>
2018-05-23 01:30:37 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/u5_57_gps/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.u5_57_gps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
